fc203b3d92c258b9dbf112f10acddffd79d78c1a
student <margarettwmboi@gmail.com>
2023-02-02 22:04:08 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>) {
+
+    var fullName: String = "margaret wamboi"
+    println(fullName)
+
+    var age: Byte = 16
+    println(age)
+
+    var phoneNumber: Int = 712345678
+    println(phoneNumber)
+
+    var citizen: Boolean = true
+    println(citizen)
+}
